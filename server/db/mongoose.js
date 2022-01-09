df38b7e2cb8949af27c85e89b227fcb84111455d
Emboiko <ed@emboiko.com>
2022-01-09 05:54:18 -0500

    mongo config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,7 @@ mongoose.connect(
   process.env.MONGODB_URL,
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
   }
 ).then(() => {
   console.log("Database connected")
