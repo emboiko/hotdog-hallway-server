da67e786a4f03db17ac7dd641d1c6de8eebe59ee
Emboiko <ed@emboiko.com>
2022-01-14 02:49:45 -0500

    revert cors options in router

--- server/router/router.js
+++ server/router/router.js
@@ -1,33 +1,9 @@
 const express = require("express")
-const cors = require('cors')
 const router = express.Router()
 const applicationRouter = require("./applicationRouter")
 const userRouter = require("./userRouter")
 
-
-const production_allowed_origins = [
-  /\.hotdog-hallway\.com$/
-]
-
-const corsOptionsDelegate = function(req, callback) {
-  var corsOptions = {}
-
-  switch (process.env.NODE_ENV.toUpperCase()) {
-      case 'TEST':
-      case 'DEVELOPMENT':
-          corsOptions["origin"] = true
-          break
-      case 'PRODUCTION':
-      default:
-          corsOptions["origin"] = production_allowed_origins
-          break
-  }
-  callback(null, corsOptions)
-}
-
-const allowCors = cors(corsOptionsDelegate)
-
-router.use("/applications", allowCors, applicationRouter)
-router.use("/users", allowCors, userRouter)
+router.use("/applications", applicationRouter)
+router.use("/users", userRouter)
 
 module.exports = router
