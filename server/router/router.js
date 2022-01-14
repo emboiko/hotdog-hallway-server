4f41905d76dea6828f962f7c93c7bc2190ecd97b
Emboiko <ed@emboiko.com>
2022-01-14 02:39:57 -0500

    CORS, cross-env scripts

--- server/router/router.js
+++ server/router/router.js
@@ -1,10 +1,33 @@
 const express = require("express")
+const cors = require('cors')
 const router = express.Router()
-
 const applicationRouter = require("./applicationRouter")
 const userRouter = require("./userRouter")
 
-router.use("/applications", applicationRouter)
-router.use("/users", userRouter)
+
+const production_allowed_origins = [
+  /\.hotdog-hallway\.com$/
+]
+
+const corsOptionsDelegate = function(req, callback) {
+  var corsOptions = {}
+
+  switch (process.env.NODE_ENV.toUpperCase()) {
+      case 'TEST':
+      case 'DEVELOPMENT':
+          corsOptions["origin"] = true
+          break
+      case 'PRODUCTION':
+      default:
+          corsOptions["origin"] = production_allowed_origins
+          break
+  }
+  callback(null, corsOptions)
+}
+
+const allowCors = cors(corsOptionsDelegate)
+
+router.use("/applications", allowCors, applicationRouter)
+router.use("/users", allowCors, userRouter)
 
 module.exports = router
