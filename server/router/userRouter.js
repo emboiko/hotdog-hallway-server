644e49eda7e01664228b5502d5c91df2de367840
Emboiko <ed@emboiko.com>
2022-02-11 14:22:14 -0500

    fix allowedUpdates for className field

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -111,7 +111,7 @@ router.post("/me/avatar", isLoggedIn, upload.single("avatar"), async (req, res) 
 
 router.patch("/me", isLoggedIn, async (req, res) => {
     const updates = Object.keys(req.body)
-    const allowedUpdates = ["discordUsername", "username", "password", "class", "race", "specialization"]
+    const allowedUpdates = ["discordUsername", "username", "password", "className", "race", "specialization"]
     const valid = updates.every((update) => allowedUpdates.includes(update))
 
     if (!valid) return res.status(400).json({error: "Invalid Updates"})
